bb0fca33c670d7be142d83c8233f3061bdc9e22e
Adnan-Bacic <adnan-bacic@hotmail.com>
2020-04-20 14:37:10 +0200

    handin 4 created

---
+++ sem2/development-enviroments/ExamHandin4_Test-Driven-Development/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExamHandin4_Test-Driven-Development")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExamHandin4_Test-Driven-Development")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("419ca206-2bd9-49db-bd14-c8444584124d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
